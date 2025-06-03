1fa9a3e48096fb7436ee5d0b4efd4c665331c2c7
bhuvabhavik <bhavikbhuva80@gmail.com>
2025-06-03 23:02:00 +0530

    bug fix

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,10 @@
+require("dotenv").config(); // <-- Load env variables
+
 const mongoose = require("mongoose");
 
 const connectDB = async ()=>{
     await mongoose.connect(
-        "mongodb+srv://bhavikbhuva080:aJqX5QuUuZjLG!q@namastenode.3l8q9sa.mongodb.net/devDosti"
+        `mongodb+srv://bhavikbhuva080:${process.env.MONGO_PS}@namastenode.3l8q9sa.mongodb.net/devDosti`
       );
 }
 
